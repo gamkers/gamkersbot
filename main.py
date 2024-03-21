939a0585e24f2da4c98f850ea7153b7d0a9bcbd5
AKASH <87299311+gamkers@users.noreply.github.com>
2024-03-21 19:43:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,35 @@
 import streamlit as st
 import google.generativeai as genai
+import base64
+from langchain_core.messages import HumanMessage, SystemMessage
 genai.configure(api_key=st.secrets["gemini_api"])
-def ai(txt):
+# def ai(txt):
     
-    for m in genai.list_models():
-        if 'generateContent' in m.supported_generation_methods:
-            print(m.name)
-    model = genai.GenerativeModel('gemini-pro')
-    response = model.generate_content("from now your name is gamkers and your a ethical hacker and cloud data engineer , your real name is akash m and reply to this in short: "+txt)
-    return response.text
+#     for m in genai.list_models():
+#         if 'generateContent' in m.supported_generation_methods:
+#             print(m.name)
+#     model = genai.GenerativeModel('gemini-pro')
+#     response = model.generate_content("from now your name is gamkers and your a ethical hacker and cloud data engineer , your real name is akash m and reply to this in short: "+txt)
+#     return response.text
 
-
-st.title("Gamkers AI assistant")
+def ai(text):
+    # Convert text to bytes
+    bytes_text = text.encode('utf-8')
+    
+    # Encode bytes to Base64
+    base64_text = base64.b64encode(bytes_text)
+    
+    encoded_text = base64_text.decode('utf-8')
+    model = ChatGoogleGenerativeAI(model="gemini-pro", convert_system_message_to_human=True, google_api_key=st.secrets["gemini_api"])
+    data = model(
+    [
+        SystemMessage(content="your task is to 1st decrypt the given base64 value and 2nd you need to create a code for the question and 3rd give a proper refrence too. Exclude how to decrypt from the out put  "),
+        HumanMessage(content= encoded_text),
+    ]
+    )
+    
+    return data.content
+st.title("Gamkers - HackerGPT")
 
 command=st.chat_input("HOW CAN I HELP YOU?")
 
