7180c57c8c5d56099d90ee0dee9d415f5edc6287
AKASH <87299311+gamkers@users.noreply.github.com>
2024-03-21 19:59:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ genai.configure(api_key=st.secrets["gemini_api"])
 #     response = model.generate_content("from now your name is gamkers and your a ethical hacker and cloud data engineer , your real name is akash m and reply to this in short: "+txt)
 #     return response.text
 
-def ai(text):
+def aicode(text):
     # Convert text to bytes
     bytes_text = text.encode('utf-8')
     
@@ -25,6 +25,21 @@ def ai(text):
     data = model(
     [
         SystemMessage(content="your task is to 1st decrypt the given base64 value and 2nd you need to create a code for the question and 3rd give a proper refrence too. Exclude how to decrypt from the out put  "),
+        HumanMessage(content= encoded_text),
+    ]
+    )
+def ai(text):
+    # Convert text to bytes
+    bytes_text = text.encode('utf-8')
+    
+    # Encode bytes to Base64
+    base64_text = base64.b64encode(bytes_text)
+    
+    encoded_text = base64_text.decode('utf-8')
+    model = ChatGoogleGenerativeAI(model="gemini-pro", convert_system_message_to_human=True, google_api_key="AIzaSyC7hJmlRUBDT6HeE1RPT7ovY1fBjgxs1jM")
+    data = model(
+    [
+        SystemMessage(content="your task is to 1st decrypt the given base64 value and 2nd you need to answer for the question and 3rd give a proper refrence too. Exclude how to decrypt from the out put  "),
         HumanMessage(content= encoded_text),
     ]
     )
@@ -40,6 +55,8 @@ if "message" not in st.session_state:
 for chat in st.session_state.message:
     with st.chat_message(chat["role"]):
         st.write(chat["message"])
+
+on = st.toggle('Generate Code')
 
 if command:
     with st.chat_message("user"):
@@ -61,9 +78,14 @@ if command:
     
     else:
         with st.chat_message("bot"):
-            data=ai(command)
-            st.write(data)
-            st.session_state.message.append({"role":"bot","message":data})
+            if on:
+                data=aicode(command)
+                st.write(data)
+                st.session_state.message.append({"role":"bot","message":data})
+            else:
+                data=ai(command)
+                st.write(data)
+                st.session_state.message.append({"role":"bot","message":data})
 
 
 
