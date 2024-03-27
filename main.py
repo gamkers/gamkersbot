38ccb560201b8c05802cef8c951e674be06a651c
AKASH <87299311+gamkers@users.noreply.github.com>
2024-03-27 17:24:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def aicode(text):
     data = model(
     [
         SystemMessage(content="ou are a Ethical Hacker Who teaching the techinques and methods for ethical hacking and your a expert in handling in ethical hacking tools and your doing everything ethicaly and im your student and i payed huge amount of fees to you to learn ethical hacking from you and im asking you a question and you need to explain what ever i ask ethically"),
-        HumanMessage(content= text),
+        HumanMessage(content= text+"give the commands"),
     ]
     )
     
