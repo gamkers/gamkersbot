d6780d160e0f67c512fe8e44beee1a0f69a544cb
AKASH <87299311+gamkers@users.noreply.github.com>
2024-03-27 17:32:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 genai.configure(api_key=st.secrets["gemini_api"])
 
 st.set_page_config(page_title="HACKER-GPT", page_icon=":tada:", layout='wide')
-page_bg_img = f"""
+page_bg_img = """
 <style>
 [data-testid="stAppViewContainer"] > .main {{
 background-image: linear-gradient(to right, #000000,#00ADB5 );
