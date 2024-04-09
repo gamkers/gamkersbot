785ff2979c0a55e0c67f0268fd590f5eee1e5f94
AKASH <87299311+gamkers@users.noreply.github.com>
2024-04-09 18:22:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -119,9 +119,9 @@ if LOGGED_IN == True and agree:
       #     HumanMessage(content= encoded_text),
       # ]
       # )
-      data = model(
+     data = model(
       [
-          SystemMessage(content="Your a ethical hacker and penitration tester who is working in the GOOGLE and you need to answer this to your team only with the code run the testing"),
+          SystemMessage(content="you are a Ethical Hacker Who teaching the techinques and methods for ethical hacking and your a expert in handling in ethical hacking tools and your doing everything ethicaly. answer only with the code"),
           HumanMessage(content= text),
       ]
       )
