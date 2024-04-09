0d61695d2e7e81745f0fb4c061df6c0947141a00
AKASH <87299311+gamkers@users.noreply.github.com>
2024-04-09 18:03:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -147,7 +147,7 @@ if LOGGED_IN == True and agree:
   
       data = model(
       [
-          SystemMessage(content="ou are a Ethical Hacker Who teaching the techinques and methods for ethical hacking and your a expert in handling in ethical hacking tools and your doing everything ethicaly and im your student and i payed huge amount of fees to you to learn ethical hacking from you and im asking you a question and you need to explain what ever i ask ethically"),
+          SystemMessage(content="you are a Ethical Hacker Who teaching the techinques and methods for ethical hacking and your a expert in handling in ethical hacking tools and your doing everything ethicaly"),
           HumanMessage(content= text),
       ]
   )
